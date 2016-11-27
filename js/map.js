b6558a4a3729ae8c3aec14b2a8f57eab9ee84d3e
Sean McNamara <admin@coder.ie>
2016-11-27 22:59:47 +0000

    Cheking if this works with github pages.

--- js/map.js
+++ js/map.js
@@ -3,9 +3,9 @@
  * @param game
  */
 function mapPreload(game) {
-    game.load.tilemap('whirlpool-map', '/assets/maps/whirlpool-map.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('whirlpool', '/assets/maps/whirlpool.jpg');
-    game.load.image('player', '/assets/images/player.png');
+    game.load.tilemap('whirlpool-map', 'assets/maps/whirlpool-map.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('whirlpool', 'assets/maps/whirlpool.jpg');
+    game.load.image('player', 'assets/images/player.png');
 }
 /**
  * Create the game map
@@ -24,4 +24,7 @@ function mapCreate(game, mapGroup) {
 
     mapGroup.add(bottomLayer);
     mapGroup.add(middleLayer);
+
+    // game.world.setBounds(0, 0, 3200, 3200);
+
 }
